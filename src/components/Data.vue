8b4e633477622e00686229fd6f5b1675ecd2a3f1
juangalbea <juangalbea@gmail.com>
2020-10-27 12:05:53 +0000

    sign of Increase

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -15,7 +15,7 @@
         <img src="../assets/trending-up.svg" alt="">
         <div>
           <div style="display: flex; align-items: center;">
-            <span class="robotofont">{{compareTwoDays(transmissions[0],transmissions[1]) == 'increase' ? "+" : "-"}}</span><h2>{{percentageChangedLastTwoDays(transmissions[0], transmissions[1])}}%</h2>
+            <span class="robotofont">{{compareTwoDays(transmissions[0],transmissions[1]) == 'Increase' ? "+" : "-"}}</span><h2>{{percentageChangedLastTwoDays(transmissions[0], transmissions[1])}}%</h2>
           </div>
           <h5>{{compareTwoDays(transmissions[0],transmissions[1])}} from previous day</h5>
         </div>
